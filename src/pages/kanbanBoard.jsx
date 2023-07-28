9d5e2245fb6de23d7fe933c8e68a99fac32a732c
Gull 2365 <jych2365@gmail.com>
2023-07-28 16:39:24 +0900

    feat: pages setup

---
+++ src/pages/kanbanBoard.jsx
@@ -0,0 +1,13 @@
+import Container from "../components/layout/container";
+import Title from "../components/Title";
+import Workflow from "../components/Workflow/Workflow";
+const KanbanBoard = () => {
+  return (
+    <Container>
+      <Title>Kanban Board</Title>
+      <Workflow>123</Workflow>
+    </Container>
+  );
+};
+
+export default KanbanBoard;
---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import Container from "../components/layout/container";
+import Title from "../components/Title";
+const Login = () => {
+  return (
+    <Container>
+      <Title>Login</Title>;
+    </Container>
+  );
+};
+
+export default Login;
