a9cb0e0ad6a4cac8bf29d41c6136e32020e8e070
Gull 2365 <jych2365@gmail.com>
2023-07-31 21:18:01 +0900

    feat: add components and complete

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,10 +1,12 @@
-import styled from "styled-components";
+import { styled } from "styled-components";
+
 const StyledTitle = styled.h1`
   font-size: 32px;
   padding: 16px 3px;
   font-weight: bold;
-  color: #fff;
+  color: #ffffff;
 `;
+
 const Title = ({ children }) => {
   return <StyledTitle>{children}</StyledTitle>;
 };
--- src/components/Workflow/Workflow.jsx
+++ src/components/Workflow/Workflow.jsx
@@ -1,18 +1,17 @@
 import { styled } from "styled-components";
 
-const Styledworkflow = styled.div`
+const StyledWorkflow = styled.div`
   width: 100%;
   display: flex;
-  justify-content: space-around;
-  background-color: #fff;
+  justify-content: space-between;
+  background-color: #ffffff;
   padding: 32px;
   box-sizing: border-box;
   border-radius: 5px;
 `;
 
 const Workflow = ({ children }) => {
-  return <Styledworkflow>{children}</Styledworkflow>;
+  return <StyledWorkflow>{children}</StyledWorkflow>;
 };
 
 export default Workflow;
- 
