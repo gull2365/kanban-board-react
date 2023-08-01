f8c9173dbd2aac8a4aa5725a60bade2871c53500
Gull 2365 <jych2365@gmail.com>
2023-08-01 16:16:19 +0900

    feat: backend 연결 테스트

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,51 @@
+import { useCallback, useState } from "react";
 import Title from "../components/Title/Title";
 import Container from "../components/layout/Container/Container";
+import axios from "axios";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+      try {
+        const {
+          data: { access_token },
+        } = await axios.post("api/auth/login", {
+          username,
+          password,
+        });
+        if (access_token) {
+          localStorage.setItem("access_token", access_token);
+          alert("로그인 통과");
+        }
+      } catch (e) {
+        alert("로그인 실패");
+      }
+    },
+    [username, password]
+  );
   return (
     <Container>
       <Title>Login</Title>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="아이디"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="암호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="로그인" />
+      </form>
     </Container>
   );
 };
